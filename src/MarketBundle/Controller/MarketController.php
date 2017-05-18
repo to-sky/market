1725660d6a0fb6be4d0e0b30f948bc65e3d295e1
tosky <dima.tolsky@gmail.com>
2017-05-19 03:30:40 +0600

    added bootstrap classes to login, products

--- src/MarketBundle/Controller/MarketController.php
+++ src/MarketBundle/Controller/MarketController.php
@@ -5,13 +5,13 @@ namespace MarketBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
+class MarketController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
     public function indexAction()
     {
-        return $this->render('MarketBundle:Default:index.html.twig');
+        return $this->render('layouts/index.html.twig');
     }
 }
