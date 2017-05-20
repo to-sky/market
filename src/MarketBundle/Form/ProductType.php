012ffa9f1d4c9d5db3cfe2e978a48614deda07ad
tosky <dima.tolsky@gmail.com>
2017-05-21 03:05:49 +0600

    changed product templates

--- src/MarketBundle/Form/ProductType.php
+++ src/MarketBundle/Form/ProductType.php
@@ -6,7 +6,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ProductType extends AbstractType
 {
@@ -15,12 +18,18 @@ class ProductType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title')
-                ->add('body')
-                ->add('imageFile', VichImageType::class, array(
-                    'required' => false
+        $builder->add('title', TextType::class)
+                ->add('body', TextareaType::class, array(
+                    'attr' => [
+                        'rows' => 8
+                    ]
                 ))
-                ->add('price');
+                ->add('imageFile', VichImageType::class, array(
+                    'label' => 'Thumbnail',
+                    'required' => false,
+                    'download_link' => false
+                ))
+                ->add('price', MoneyType::class);
                 // ->add('publishedAt', DateTimeType::class, [
                 //     'placeholder' => array(
                 //         'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
